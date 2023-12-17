a929b11c1cca22d9387e19c1f746e9cf32b7e6c7
Eric Zheng <ezheng118@gmail.com>
2023-12-17 00:24:15 -0500

    solutions for day 2 and 3

---
+++ day2.rs
@@ -0,0 +1,67 @@
+use std::env;
+use std::fs::File;
+use std::io::Read;
+use std::io;
+
+fn read_to_string(filename: &str) -> io::Result<String>
+{
+    let mut file = File::open(&filename)?;
+    let mut text = String::new();
+    file.read_to_string(&mut text)?;
+    Ok(text)
+}
+
+fn main()
+{
+    let name = env::args().nth(1).expect("need a file name");
+
+    let text = read_to_string(&name).expect("bad file");
+
+    /* soln for part 1
+    let reds = 12u32;
+    let greens = 13u32;
+    let blues = 14u32;
+    */
+
+    let ans = text
+        .lines()
+        .filter(|line| !line.is_empty())
+        .map(|line| line.split(' ').collect::<Vec<&str>>()) // split into tokens and toss "Game"
+        .fold(0u32, |acc, vec| {
+            let val = vec[1].replace(":","").parse::<u32>().unwrap();
+            let rest = vec[2..].chunks(2);
+
+            /* soln for part 1
+            for pair in rest {
+                let color = pair[1].replace(",", "").replace(";","");
+                let amt = pair[0].parse::<u32>().unwrap();
+                match color.as_str() {
+                    "red" => if amt > reds { return acc },
+                    "blue" => if amt > blues { return acc },
+                    "green" => if amt > greens { return acc },
+                    _ => panic!("unexpected input for game {}", val)
+                };
+            }
+
+            acc + val
+            */
+
+            // soln for part 2
+            let mut red_needed = 0u32;
+            let mut blue_needed = 0u32;
+            let mut green_needed = 0u32;
+            for pair in rest {
+                let color = pair[1].replace(",", "").replace(";","");
+                let amt = pair[0].parse::<u32>().unwrap();
+                match color.as_str() {
+                    "red" => if amt > red_needed { red_needed = amt; },
+                    "blue" => if amt > blue_needed { blue_needed = amt; },
+                    "green" => if amt > green_needed { green_needed = amt; },
+                    _ => panic!("unexpected input for game {}", val)
+                };
+            }
+
+            acc + red_needed * blue_needed * green_needed
+        });
+    println!("{}", ans);
+}
---
+++ day3.rs
@@ -0,0 +1,155 @@
+use std::env;
+use std::fs::File;
+use std::io::Read;
+use std::io;
+
+use std::collections::HashMap;
+use std::rc::Rc;
+use std::cell::RefCell;
+
+fn read_to_string(filename: &str) -> io::Result<String>
+{
+    let mut file = File::open(&filename)?;
+    let mut text = String::new();
+    file.read_to_string(&mut text)?;
+    Ok(text)
+}
+
+fn main()
+{
+    let name = env::args().nth(1).expect("need a file name");
+
+    let text = read_to_string(&name).expect("bad file");
+
+    let num_rows = text.lines().count();
+    let num_cols = text.lines().nth(0).expect("empty file").chars().count();
+
+    let mut collect: u32 = 0;
+    let mut stop_collect = false;
+    let mut trailing: Vec<usize> = vec![];
+
+    let mut sym_indices: Vec<(usize, char)> = vec![];
+    let mut num_map = HashMap::new();
+    let mut accumulate: u32 = 0;
+
+    for (idx, letter) in text.replace('\n', "").char_indices()
+    {
+        match letter.to_digit(10)
+        {
+            Some(n) =>
+            {
+                collect = collect * 10 + n;
+                trailing.push(idx);
+
+                // make sure that you don't read past the end of the line
+                if idx - num_cols * (idx/num_cols) == num_cols - 1
+                {
+                    stop_collect = true;
+                }
+            },
+            None =>
+            {
+                if collect > 0
+                {
+                    stop_collect = true;
+                }
+                if letter != '.'
+                {
+                    sym_indices.push((idx, letter));
+                }
+            }
+        }
+
+        if stop_collect
+        {
+            let val = Rc::new(RefCell::new(Some(collect)));
+            // this is an ugly while loop, but I don't know
+            // how I'm actually supposed to write this so
+            loop
+            {
+                match trailing.pop()
+                {
+                    Some(i) => num_map.insert(i, val.clone()),
+                    None => break
+                };
+            }
+            collect = 0;
+            stop_collect = false;
+        }
+    }
+
+    // soln for part 1
+    /*
+    for (idx, _) in &sym_indices
+    {
+        let row = idx / num_cols;
+        let rows = [if row > 0 {Some(row-1)} else {None}, Some(row), if row < num_rows-1 {Some(row+1)} else {None}];
+
+        let col = idx - num_cols * row;
+        let cols = [if col > 0 {Some(col-1)} else {None}, Some(col), if col < num_cols-1 {Some(col+1)} else {None}];
+
+        for i in rows.iter().filter(|v| v.is_some()).map(|v| v.unwrap())
+        {
+            for j in cols.iter().filter(|v| v.is_some()).map(|v| v.unwrap())
+            {
+                match num_map.get(&(i * num_cols + j))
+                {
+                    Some(val) => {
+                        let something = &*val;
+                        let refc = Rc::try_unwrap(something.into()).unwrap_or_else(|_| panic!("failed to unwrap Rc"));
+                        let inside = refc.replace(None);
+                        match inside {
+                            Some(num) => accumulate += num,
+                            None => ()
+                        }
+                    },
+                    None => ()
+                }
+            }
+        }
+    }
+    */
+
+    // this is roughly the same as part 1, just with a few extra checks to see if something is a
+    // gear or not
+    for (idx, letter) in &sym_indices
+    {
+        if letter != &'*' { continue; }
+
+        let mut part_nums: Vec<u32> = vec![];
+
+        let row = idx / num_cols;
+        let rows = [if row > 0 {Some(row-1)} else {None}, Some(row), if row < num_rows-1 {Some(row+1)} else {None}];
+
+        let col = idx - num_cols * row;
+        let cols = [if col > 0 {Some(col-1)} else {None}, Some(col), if col < num_cols-1 {Some(col+1)} else {None}];
+
+        for i in rows.iter().filter(|v| v.is_some()).map(|v| v.unwrap())
+        {
+            for j in cols.iter().filter(|v| v.is_some()).map(|v| v.unwrap())
+            {
+                match num_map.get(&(i * num_cols + j))
+                {
+                    Some(val) => {
+                        let something = &*val;
+                        let refc = Rc::try_unwrap(something.into()).unwrap_or_else(|_| panic!("failed to unwrap Rc"));
+                        let inside = refc.replace(None);
+                        match inside {
+                            Some(num) => part_nums.push(num),
+                            None => ()
+                        }
+                    },
+                    None => ()
+                }
+            }
+        }
+
+        if part_nums.len() == 2
+        {
+            accumulate += part_nums[0] * part_nums[1];
+        }
+    }
+
+    println!("{}", accumulate);
+}
+
